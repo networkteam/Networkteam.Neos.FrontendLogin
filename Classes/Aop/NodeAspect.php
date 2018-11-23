33c2caa9bb46e7966698a228bc6f866afb734511
Kai Möller <kai.moeller@networkteam.com>
2018-11-23 12:54:35 +0100

    Task: configure aop

--- Classes/Aop/NodeAspect.php
+++ Classes/Aop/NodeAspect.php
@@ -5,6 +5,8 @@ namespace Networkteam\Neos\FrontendLogin\Aop;
  *  (c) 2018 networkteam GmbH - all rights reserved
  ***************************************************************/
 
+use Neos\ContentRepository\Domain\Utility\NodePaths;
+use Neos\Error\Messages\Error;
 use Neos\Flow\Annotations as Flow;
 use Neos\Flow\AOP\JoinPointInterface;
 
@@ -15,16 +17,24 @@ class NodeAspect
 {
 
     /**
-     * \Neos\ContentRepository\TypeConverter\NodeConverter::convertFrom
-     * \Neos\ContentRepository\Domain\Service\Context::getNode
-     */
-
-    /**
-     * @Flow\AfterReturning("method(Neos\ContentRepository\Domain\Service\Context->getNode())")
+     * @Flow\After("method(Neos\ContentRepository\TypeConverter\NodeConverter->convertFrom())")
      * @param JoinPointInterface $joinPoint
      */
     public function doSomething(JoinPointInterface $joinPoint)
     {
-        $foo = 'bar';
+        $result = $joinPoint->getResult();
+
+        // Could not convert array to Node object because the node "%s" does not exist.
+        if ($result instanceof Error && $result->getCode() === 1370502328) {
+            $methodArguments = $joinPoint->getMethodArguments();
+            $source = $methodArguments['source'];
+            $nodePathAndContext = NodePaths::explodeContextPath($source);
+            $nodePath = $nodePathAndContext['nodePath'];
+
+            /**
+             * TODO: prüfen ob nodepath Teil von oder selber MemberAreaRoot ist
+             * Wenn ja, dann finde MemberAreaRoot und nehme login node property und leite weiter zu login seite
+             */
+        }
     }
 }
