38d7188c98de7c3f43a36afd28ea3cbe0ba85da5
Kai Möller <kai.moeller@networkteam.com>
2022-08-30 09:57:42 +0200

    add "addUser" method in UserService
    The "addUser" method prevents creating user workspace.

--- Classes/Service/UserService.php
+++ Classes/Service/UserService.php
@@ -48,5 +48,54 @@ class UserService extends \Neos\Neos\Domain\Service\UserService
         );
     }
 
+    /**
+     * Adds a user whose User object has been created elsewhere. A workspace is NOT created.
+     *
+     * This method basically "creates" a user like createUser() would, except that it does not create the User
+     * object itself. If you need to create the User object elsewhere, for example in your ActionController, make sure
+     * to call this method for registering the new user instead of adding it to the PartyRepository manually.
+     *
+     * @param string $username The username of the user to be created.
+     * @param string $password Password of the user to be created
+     * @param User $user The pre-built user object to start with
+     * @param array $roleIdentifiers A list of role identifiers to assign
+     * @param string $authenticationProviderName Name of the authentication provider to use. Example: "Neos.Neos:Backend"
+     * @return User The same user object
+     * @api
+     */
+    public function addUser($username, $password, User $user, array $roleIdentifiers = null, $authenticationProviderName = null)
+    {
+        if ($roleIdentifiers === null) {
+            $roleIdentifiers = ['Networkteam.Neos.FrontendLogin:FrontendUser'];
+        }
+        $roleIdentifiers = $this->normalizeRoleIdentifiers($roleIdentifiers);
+        $account = $this->accountFactory->createAccountWithPassword(
+            $username,
+            $password,
+            $roleIdentifiers,
+            $authenticationProviderName ?: $this->defaultAuthenticationProviderName
+        );
+        $this->partyService->assignAccountToParty($account, $user);
+
+        $this->partyRepository->add($user);
+        $this->accountRepository->add($account);
+
+        $this->emitFrontendUserCreated($user);
+
+        return $user;
+    }
+
+
+    /**
+     * Signals that a new frontend user, including a new account has been created.
+     *
+     * @param User $user The created user
+     * @return void
+     * @Flow\Signal
+     * @api
+     */
+    public function emitFrontendUserCreated(User $user)
+    {
+    }
 }
 
